f55edc26ae4b32c158edd04c804cbf3ce825f3fa
d3Yv3r <deyverson@astrotech.solutions>
2024-07-05 16:25:52 -0300

    desafio Deyverson Medeiros

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,19 +3,30 @@ import React from "react";
 import "./style.css";
 
 import headerImg from "../../assets/images/image-1.webp";
-
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
       <div className="header-content container">
         <div className="header-image">
-          <img src={headerImg} alt="Imagem ilustrativa de uma pessoa comemorando" />
+          <img
+            src={headerImg}
+            alt="Imagem ilustrativa de uma pessoa comemorando"
+          />
         </div>
         <div className="header-description">
-          <h1>Encontre os <span>melhores artigos</span> de programação em um só lugar</h1>
-          <p>Explore o topo da programação em um só lugar! Seu destino único para dicas e tendências atuais.</p>
-          <a href="#article" className="button">Buscar artigos</a>
+          <h1>
+            Encontre os <span className="bestsArticle">melhores artigos</span>{" "}
+            de programação em um só lugar
+          </h1>
+          <p>
+            Explore o topo da programação em um só lugar! Seu destino único para
+            dicas e tendências atuais.
+          </p>
+          <Link to="/post" className="button">
+            Buscar artigos
+          </Link>
         </div>
       </div>
     </header>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ArticleSection from "../ArticleSection/ArticleSection";
 import ContactSection from "../ContactSection/ContactSection";
 
 const Main = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
+
   return (
     <main>
-      <ArticleSection />
+      <ArticleSection data={data} />
       <ContactSection />
     </main>
   );
